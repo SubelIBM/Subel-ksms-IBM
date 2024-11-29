2b4aa3424d941aff462bbf7f4d3b857b0be7f7fa
subel-biswas <subel.bca@gmail.com>
2024-11-29 19:22:43 +0530

    first commit

---
+++ Day 1/importingsort.js
@@ -0,0 +1,6 @@
+const { sort1, sort2 } = require("./sortingexport");
+
+console.log("Importing sorting function");
+
+sort1();
+sort2();
---
+++ Day 1/sortingexport.js
@@ -0,0 +1,12 @@
+function sort1() {
+  console.log("Sorting 1 ... ");
+}
+
+function sort2() {
+  console.log("Sorting 2 ... ");
+}
+
+module.exports = {
+  sort1,
+  sort2,
+};
